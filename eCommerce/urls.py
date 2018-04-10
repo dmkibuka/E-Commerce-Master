e7299ba0f0e7720b8c52bd1e751441b70c128318
David <dmkibuka@yahoo.com>
2018-04-09 20:26:14 -0400

    Homepage, Aboutpage, contactpage, base template and components

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -19,7 +19,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
+from .views import home_page, about_page, contact_page
+
+
 urlpatterns = [
+    path('', home_page, name='home'),
+    path('about/', about_page, name='about'),
+    path('contact/', contact_page, name='contact'),
     path('admin/', admin.site.urls),
 ]
 
