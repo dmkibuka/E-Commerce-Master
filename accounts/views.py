7b53b5819e61500f761bf7d501c7e4c4811ac3b0
David <dmkibuka@yahoo.com>
2018-04-11 00:24:47 -0400

    Guest Checkout Profile added to checkout process

--- accounts/views.py
+++ accounts/views.py
@@ -1,34 +1,53 @@
 from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
+from django.utils.http import is_safe_url
 
 
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, GuestForm
+from .models import GuestEmail
+
+
+def guest_register_view(request):
+    form = GuestForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_ or next_post or None
+    if form.is_valid():
+        email       = form.cleaned_data.get("email")
+        new_guest_email = GuestEmail.objects.create(email=email)
+        request.session['guest_email_id'] = new_guest_email.id
+        if is_safe_url(redirect_path, request.get_host()):
+            return redirect(redirect_path)
+        else:
+            return redirect("/register/")
+    return redirect("/register/")
+
 
 def login_page(request):
     form = LoginForm(request.POST or None)
     context = {
         "form": form
     }
-    print("User logged in")
-    #print(request.user.is_authenticated())
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_ or next_post or None
     if form.is_valid():
-        print(form.cleaned_data)
         username  = form.cleaned_data.get("username")
         password  = form.cleaned_data.get("password")
         user = authenticate(request, username=username, password=password)
-        print(user)
-        #print(request.user.is_authenticated())
         if user is not None:
-            #print(request.user.is_authenticated())
             login(request, user)
-            # Redirect to a success page.
-            #context['form'] = LoginForm()
-            return redirect("/")
+            if is_safe_url(redirect_path, request.get_host()):
+                return redirect(redirect_path)
+            else:
+                return redirect("/")
         else:
             # Return an 'invalid login' error message.
             print("Error")
-
     return render(request, "account/login.html", context)
 
 
