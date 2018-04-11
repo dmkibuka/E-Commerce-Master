d4f50fd7b85358daddfb97125f6c4eacd9467da4
David <dmkibuka@yahoo.com>
2018-04-10 21:33:13 -0400

    Created Checkout View

--- carts/urls.py
+++ carts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
         cart_home,
         cart_update,
+        checkout_home,
         )
 
 app_name = 'carts'
@@ -11,4 +12,5 @@ app_name = 'carts'
 urlpatterns = [
     path('', cart_home, name='home'),
     path('update/', cart_update, name='update'),
+    path('checkout/', checkout_home, name='checkout'),
 ]
