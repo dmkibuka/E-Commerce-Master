c46a34b5ab7553b4451ab5f17b47244ae2c2a790
David <dmkibuka@yahoo.com>
2018-04-11 05:35:13 -0400

    Reuse Addresses for Checkout

--- addresses/urls.py
+++ addresses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import checkout_address_create_view
+from .views import checkout_address_create_view, checkout_address_reuse_view
 
 app_name = 'addresses'
 
 
 urlpatterns = [
     path('checkout/create/', checkout_address_create_view, name='checkout_address_create'),
+    path('checkout/reuse/', checkout_address_reuse_view, name='checkout_address_reuse'),
 ]
